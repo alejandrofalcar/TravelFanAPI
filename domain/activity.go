1230e4a52e5c65a1b2d4963ccecb1f4f38908c70
Alejandro <afalcon@merkinsio.com>
2023-04-29 18:05:27 +0200

    Minor changes

--- domain/activity.go
+++ domain/activity.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Activity struct {
-	ID        uint           `gorm:"column:ID;primary_key;AUTO_INCREMENT;NOT NULL"`
+	ID        string         `gorm:"column:ID;primary_key;AUTO_INCREMENT;NOT NULL"`
 	Date      string         `gorm:"column:date"`
 	Image     string         `gorm:"column:image"`
 	Type      string         `gorm:"column:type"`
@@ -17,8 +17,6 @@ type Activity struct {
 	Latitude  float64        `gorm:"column:latitude"`
 	Longitude float64        `gorm:"column:longitude"`
 	Street    string         `gorm:"column:street"`
-	File      string         `gorm:"column:file"`
-	FileData  []byte         `gorm:"column:file_data"`
 	TripID    uint           `gorm:"column:trip_id;not null"`
 	Trip      Trip           `gorm:"foreignKey:TripID"`
 	CreatedAt time.Time      `gorm:"autoCreateTime"`
@@ -50,9 +48,6 @@ func (d *Activity) Modify(mod Activity, file []byte) {
 	}
 	if mod.Street != d.Street {
 		d.Street = mod.Street
-	}
-	if mod.File != d.File {
-		d.File = mod.File
 	}
 	if mod.TripID != d.TripID {
 		d.TripID = mod.TripID
